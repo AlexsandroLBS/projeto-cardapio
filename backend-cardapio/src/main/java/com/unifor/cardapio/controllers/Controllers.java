8a90ab490567981e38f84c4699ef612ac93cfaf4
Eduardo Barroso <dev@edbar.xyz>
2024-10-13 22:06:07 -0300

    refactor(server): restaura Controllers para o estado de teste

--- backend-cardapio/src/main/java/com/unifor/cardapio/controllers/Controllers.java
+++ backend-cardapio/src/main/java/com/unifor/cardapio/controllers/Controllers.java
@@ -2,13 +2,12 @@ package com.unifor.cardapio.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class Controllers {
 
-	@GetMapping(value = { "/", "/login", "/s/**" })
-	public ModelAndView index() {
-		return new ModelAndView("forward:/index.html");
+	@GetMapping("/")
+	public String sayHello() {
+		return "Hello from my Spring Boot server!";
 	}
 }
